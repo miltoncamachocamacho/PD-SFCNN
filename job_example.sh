bfc8cc66014886b376d9ce726e1f5fe2ac374cf3
miltoncamachocamacho <150370230+miltoncamachocamacho@users.noreply.github.com>
2024-02-23 10:27:34 -0700

    Update job_example.sh

--- job_example.sh
+++ job_example.sh
@@ -10,8 +10,34 @@
 
 source ~/miniconda3/bin/activate myenv
 
-#python pd_sfcn.py model_name True model_loaded epochs random_split learning_rate False ad,fa,md,rd True False /home/milton.camachocamach/scratch/second_project/ 0.9 1 > log.txt
 python pd_sfcn.py \
+#model name
+model_name \
+#is it the first run?
+True \
+#name of loaded model if any
+model_loaded \
+#n of epochs
+60 \
+#random split
+1 \
+#learning rate
+0.01 \
+#learning decay
+0.001 \
+#run augmentation
+False \
+#extension of DTI data to use for the run
+ad,fa,md,rd \
+#use age and sex?
+True \
+#add regularization
+False \
+#batch size
+4 > log.txt
+
+#for explainability also run
+python exp.py \
 #model name
 model_name \
 #is it the first run?
